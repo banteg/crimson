003e8740c89ce335061fa48930da6169e8ec70cb
banteg <4562643+banteg@users.noreply.github.com>
2026-01-20 03:00:01 +0000

    feat(view): add weapon icon preview

--- src/crimson/views/__init__.py
+++ src/crimson/views/__init__.py
@@ -11,6 +11,7 @@ def _register_builtin_views() -> None:
     from . import terrain as _terrain  # noqa: F401
     from . import projectiles as _projectiles  # noqa: F401
     from . import bonuses as _bonuses  # noqa: F401
+    from . import wicons as _wicons  # noqa: F401
 
 
 _register_builtin_views()
