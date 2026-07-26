4a75ad66ea0b26e728c472094022ee9278570611
banteg <4562643+banteg@users.noreply.github.com>
2026-07-26 18:58:17 +0400

    feat(match): recover reserved color initializer

---
+++ tools/match/scratches/cfltcvt_init_3/scratch.cpp
@@ -0,0 +1,11 @@
+#include "crimsonland_types.h"
+
+extern "C" effect_color_t reserved_color_4871b8;
+
+extern "C" void __cfltcvt_init_3(void)
+{
+    reserved_color_4871b8.r = 1.0f;
+    reserved_color_4871b8.g = 0.0f;
+    reserved_color_4871b8.b = 0.0f;
+    reserved_color_4871b8.a = 1.0f;
+}
